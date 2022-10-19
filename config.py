e2b2aac6e729f6a1ce24e305af8bd1da6550096f
Idan Ludmir <chinocolu@gmail.com>
2022-10-20 02:36:32 +0300

    Changes in config after all runs

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-CLASSES = ['Bovidae', 'Cervidae', 'Canidae', 'Felidae', 'Mustelidae']
+#CLASSES = ['Bovidae', 'Cervidae', 'Canidae', 'Felidae', 'Mustelidae']
+#CLASSES = ['Bovidae', 'Cervidae', 'Canidae', 'Felidae', 'Mustelidae', 'Giraffidae']
+CLASSES = ['Bovidae', 'Cervidae', 'Canidae', 'Felidae', 'Mustelidae', 'Giraffidae', 'Pteropodidae', 'Procaviidae', 'Cercopithecidae', 'Delphinidae', 'Hyaenidae', 'Ursidae']
 PRETRAINING = 0
 FINE_TUNING = 1
 
@@ -12,23 +14,23 @@ class Config:
         self.checkpoint_dir = "checkpoint/"
 
         if self.mode == PRETRAINING:
-            self.batch_size = 10
+            #self.batch_size = 10
+            self.batch_size = 32
             self.nb_epochs_per_saving = 1
             self.pin_mem = True
             self.num_cpu_workers = 1
-            self.nb_epochs = 50
+            self.nb_epochs = 100
             self.cuda = True
             # Optimizer
             self.lr = 1e-4
             self.weight_decay = 5e-5
-            # Hyperparameters for our y-Aware InfoNCE Loss
-            self.sigma = 1 # depends on the meta-data at hand
             self.temperature = 0.1
             self.model = "DenseNet"
+            #self.loss = "NTXent"
+            self.loss = "SupCon"
 
             #self.pretrained_path = "DenseNet121_BHB-10K_yAwareContrastive.pth"
-            #self.pretrained_path = "checkpoint/ntxent_Contrastive_MRI_epoch_21_80_80_32.pth"
-            #self.pretrained_path = "checkpoint/ntxent_Contrastive_MRI_epoch_20.pth"
+            self.pretrained_path = "checkpoint/NTXent_epoch_45_OpenNeuro_with_BHB10K.pth"
 
         elif self.mode == FINE_TUNING:
             ## We assume a classification task here
@@ -42,7 +44,13 @@ class Config:
             self.lr = 1e-4
             self.weight_decay = 5e-5
 
-            #self.pretrained_path = "checkpoint/ntxent_Contrastive_MRI_epoch_50_80_80_32_mam.pth"
-            #self.pretrained_path = "checkpoint/ntxent_Contrastive_MRI_epoch_44_5_classes_SupCon.pth"           
-            self.num_classes = 5
+            #self.pretrained_path = "checkpoint/NTXent_epoch_45_OpenNeuro_with_BHB10K.pth" 
+            #self.pretrained_path = "checkpoint/SupCon_epoch_100_MaMI.pth"  
+            #self.pretrained_path = "checkpoint/SupCon_epoch_52_all.pth"  
+            self.pretrained_path = "checkpoint/fine_tune_epoch_100_all.pth"  
+            #self.pretrained_path = "checkpoint/NTXent_epoch_86_MaMI.pth"  
+            #self.pretrained_path = "checkpoint/NTXent_epoch_89_MaMI_bs32.pth"  
+
+            #self.num_classes = 5
+            self.num_classes = 12
             self.model = "DenseNet"
